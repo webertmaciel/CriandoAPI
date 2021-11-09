7e78128d12a21ab3f50dd49c46c6c2301ad098a6
webert maciel <webert_maciel@hotmail.com>
2021-11-08 23:49:13 -0300

    new commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const posts = require('./model/posts')
+
+const app = express();
+
+
+
+app.get("/all", (req, res) => {
+
+    res.json(JSON.stringify(posts.getAll()))
+
+
+});
+app.post("/new", bodyParser.json(), (req, res) => {
+
+    let title = req.body.title;
+    let description = req.body.description;
+    posts.newPost(title, description);
+
+
+    res.send("Post adicionado")
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server running on pot', PORT)
+});
+
