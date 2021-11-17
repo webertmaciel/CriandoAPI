4a7fc896401975bb9d2d8c14c55e27e58c5a64d6
webert maciel <webert_maciel@hotmail.com>
2021-11-17 18:17:45 -0300

    aaaaaa
    aaaaaaaa

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,8 +21,18 @@ function updatePosts() {
         document.getElementById("posts").innerHTML = postElements
     })
 }
-function mewPost() {
+function newPost() {
     let title = document.getElementById("title").value
     let description = document.getElementById("desc").value
-    console.log(title, description)
+    let post = { title, description }
+
+    const options = {
+        method: "POST",
+        headers: new Headers({ 'content-type': 'application/json' }),
+        body: JSON.stringify(post)
+    }
+    fetch("http://localhost:3000/api/new", options).then(res => {
+        console.log(res);
+        updatePosts();
+    })
 }
